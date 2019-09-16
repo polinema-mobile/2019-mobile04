c20d1cf6c19e378bbc57d747cf0071a134673258
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-09-16 20:25:01 +0700

    Parceable (Masih error)

--- Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -2,14 +2,46 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import model.User;
 
 public class ParcelableActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
+
+    private String username;
+    private String name;
+    private int age;
+
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
     }
 
+    public void handleSubmit(View view) {
+
+        username = usernameInput.getText().toString();
+        name = nameInput.getText().toString();
+//        age = Integer.parseInt(ageInput.getText().toString());
+
+        user = new User(username, name, age);
+
+        Intent intent = new Intent(this, ProfileParcelableActivity.class);
+        intent.putExtra("user", user);
+
+        startActivity(intent);
+    }
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -2,9 +2,17 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import model.User;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +20,24 @@ public class ProfileParcelableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_parcelable);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nameText= findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
+
+        Intent intent = getIntent();
+        User parcel = intent.getParcelableExtra("user");
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
 
             // TODO: display value here
+            String username = parcel.getUsername();
+            String name = parcel.getName();
+//            int age = Integer.parseInt(ageText.getText().toString());
+
+            usernameText.setText(username);
+            nameText.setText(name);
+//            ageText.setText(String.valueOf(extras.getInt(age)));
         }
     }
 }
